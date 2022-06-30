05ea574ca11bc5bbf4030a98992cbe5373084743
leule3 <leulehaylay3@gmail.com>
2022-06-30 14:30:51 -0700

    1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include <stdio.h>                                                                                                         
-                                                                                                                           
-/**                                                                                                                        
- * main - Entry point                                                                                                      
- *                                                                                                                         
- * Return: Always 0 (Success)                                                                                              
-*/                                                                                                                        
-                                                                                                                           
-int main(void)                                                                                                             
-{                                                                                                                          
-	        puts("\"Programming is like building a multilingual puzzle");                                                      
-		        return (0);                                                                                                        
-}  I
+nclude <stdio.h>                                                                         
+                                                                                           
+/**                                                                                        
+ *  * main - Entry point                                                                      
+ *   *                                                                                         
+ *    * Return: Always 0 (Success)                                                              
+ *     */                                                                                        
+                                                                                           
+int main(void)                                                                             
+{                                                                                          
+	        puts("\"Programming is like building a multilingual puzzle");
+		return (0);I
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
  * Return:Always 0 (success)
@@ -11,3 +11,4 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
