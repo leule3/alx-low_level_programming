d87ae937325b6f7ffc3f23a6e8c8e7539172453a
leule3 <leulehaylay3@gmail.com>
2022-07-01 12:59:56 -0700

    m

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -entry prints reverse alphabet
+ * Return: always succes
+ */
+int main(void)
+{
+	char c;
+	for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
+
