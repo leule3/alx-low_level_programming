7d77e4bf2b47866ac5780174fb919a0d28dea259
leule3 <leulehaylay3@gmail.com>
2022-07-05 12:23:44 -0700

    l

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ *main- this is the begning fo the end of me
+ *Return:0 shit
+ */
+int main(void)
+{
+putchar ("_");
+putchar ("p");
+putchar ("u");
+putchar ("t");
+putchar ("c");
+putchar ("h");
+putchar ("a");
+putchar ("r");
+putchar ("\n");
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+beletu tebelu
