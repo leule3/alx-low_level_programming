bc81145a1993eedf52f46075551526e466fc79d3
leule3 <leulehaylay3@gmail.com>
2022-07-07 06:26:35 -0700

    albino

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,15 @@
 #include "main.h"
+
 /**
- *main- felege sewyew
- *Return: this shit
+ *main- tests function that prints if integer is positive or negative
+ * Return:0
  */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
 }
