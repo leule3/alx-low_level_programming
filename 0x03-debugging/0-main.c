9e6f7cbc253e8cfdad9e6f7656feb684fa63e3e3
leule3 <leulehaylay3@gmail.com>
2022-07-07 07:10:19 -0700

    athon

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- *main- tests function that prints if integer is positive or negative
- * Return:0
+ * * main - tests function that prints if integer is positive or negative
+ * * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -13,3 +14,4 @@ int main(void)
 
 	return (0);
 }
+
