cbc8f2d58587a6f896ef2145deb4ddaffef8ddb5
leule3 <leulehaylay3@gmail.com>
2022-07-07 06:11:29 -0700

    manm

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,31 +1,6 @@
 #ifndef MAIN_H
+
 #define MAIN_H
-
-#include <stdio.h>
-
 void positive_or_negative(int i);
-#include <stdio.h>
-
-/**
- * * main - causes an infinite loop
- * * Return: 0
- */
-
-int main(void)
-{
-	int i;
-
-	printf("Infinite loop incoming :(\n");
-
-	i = 0;
-
-	while (i < 10)
-	{
-		putchar(i);
-	}
-
-	printf("Infinite loop avoided! \\o/\n");
-
-	return (0);
-}
+int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
